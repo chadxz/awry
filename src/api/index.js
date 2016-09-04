c79b09c31f1d31b5d0a2fc0a2eee1eee8c5506ac
Chad McElligott <chad.mcelligott@gmail.com>
2016-09-04 00:43:12 -0500

    Add Asterisk API

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 'use strict';
-const ApplicationsAPI = require('./Applications');
+const ApplicationsAPI = require('./ApplicationsAPI');
+const AsteriskAPI = require('./AsteriskAPI');
 
 class API {
   constructor(params) {
     this.applications = new ApplicationsAPI(params);
+    this.asterisk = new AsteriskAPI(params);
   }
 }
 
---
+++ test/api/AsteriskAPI.spec.js
@@ -0,0 +1,364 @@
+'use strict';
+const AsteriskAPI = require('../../src/api/AsteriskAPI');
+const nock = require('nock');
+
+describe('the Asterisk API', () => {
+
+  beforeEach(() => {
+    nock.disableNetConnect();
+  });
+
+  afterEach(() => {
+    nock.enableNetConnect();
+  });
+
+  describe('getObject method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .get('/ari/asterisk/config/dynamic/fooClass/barType/baz')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.getObject({
+        configClass: 'fooClass',
+        objectType: 'barType',
+        id: 'baz'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('updateObject method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .put('/ari/asterisk/config/dynamic/fooClass/barType/baz', {
+          fields: [
+            { field1: 'field1val' },
+            { field2: 'field2val' }
+          ]
+        })
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.updateObject({
+        configClass: 'fooClass',
+        objectType: 'barType',
+        id: 'baz',
+        fields: [
+          { field1: 'field1val' },
+          { field2: 'field2val' }
+        ]
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('deleteObject method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .delete('/ari/asterisk/config/dynamic/fooClass/barType/baz')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.deleteObject({
+        configClass: 'fooClass',
+        objectType: 'barType',
+        id: 'baz'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('getInfo method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .get('/ari/asterisk/info')
+        .query({ only: 'system,build' })
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.getInfo({
+        only: ['system', 'build']
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('listModules method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .get('/ari/asterisk/modules')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.listModules().then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('getModule method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .get('/ari/asterisk/modules/myModule')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.getModule({
+        moduleName: 'myModule'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('loadModule method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .post('/ari/asterisk/modules/my%20Module')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.loadModule({
+        moduleName: 'my Module'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('unloadModule method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .delete('/ari/asterisk/modules/my%20Module')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.unloadModule({
+        moduleName: 'my Module'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('reloadModule method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .put('/ari/asterisk/modules/my%20Module')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.reloadModule({
+        moduleName: 'my Module'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('listLogChannels method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .get('/ari/asterisk/logging')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.listLogChannels().then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('addLog method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .post('/ari/asterisk/logging/my%20log%20channel')
+        .query({ configuration: "i have no idea what i'm doing"})
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.addLog({
+        logChannelName: 'my log channel',
+        configuration: "i have no idea what i'm doing"
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('deleteLog method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .delete('/ari/asterisk/logging/my%20log%20channel')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.deleteLog({
+        logChannelName: 'my log channel'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('rotateLog method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .put('/ari/asterisk/logging/my%20log%20channel/rotate')
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.rotateLog({
+        logChannelName: 'my log channel'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('getGlobalVariable method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .get('/ari/asterisk/variable')
+        .query({ variable: 'something cool' })
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.getGlobalVariable({
+        variable: 'something cool'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+
+  describe('setGlobalVariable method', () => {
+
+    // TODO: add more tests
+    it('makes the right request', () => {
+      const mock = nock('http://fake.local')
+        .post('/ari/asterisk/variable')
+        .query({
+          variable: 'something cool',
+          value: 'do what now'
+        })
+        .reply(200, { foo: 'bar' });
+
+      const api = new AsteriskAPI({
+        baseUrl: 'http://fake.local/ari',
+        username: 'user',
+        password: '1234'
+      });
+
+      return api.setGlobalVariable({
+        variable: 'something cool',
+        value: 'do what now'
+      }).then(() => {
+        mock.done();
+      });
+    });
+  });
+});
