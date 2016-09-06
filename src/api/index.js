db008c6b02d89dbb0c3f2984428a89412e72916c
Chad McElligott <chad.mcelligott@gmail.com>
2016-09-05 22:13:16 -0500

    Add comments and tests for higher level components

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,16 @@ const AsteriskAPI = require('./AsteriskAPI');
 const BridgesAPI = require('./BridgesAPI');
 
 class API {
+  /**
+   * Creates a new awry API instance, providing clients for all available
+   * Asterisk ARI endpoints.
+   *
+   * @param {string} params.username The username to send with the request.
+   * @param {string} params.password The password to send with the request.
+   * @param {string} params.baseUrl The base url, without trailing slash,
+   *  of the root Asterisk ARI endpoint. i.e. 'http://myserver.local:8088/ari'.
+   * @constructor
+   */
   constructor(params) {
     this.applications = new ApplicationsAPI(params);
     this.asterisk = new AsteriskAPI(params);
---
+++ test/api/index.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+const API = require('../../src/api');
+const assert = require('power-assert');
+const ApplicationsAPI = require('../../src/api/ApplicationsAPI');
+const AsteriskAPI = require('../../src/api/AsteriskAPI');
+const BridgesAPI = require('../../src/api/BridgesAPI');
+
+describe('API constructed instance', () => {
+
+  let api;
+
+  beforeEach(() => {
+    api = new API({
+      username: 'username',
+      password: 'password',
+      baseUrl: 'http://127.0.0.1/ari'
+    });
+  });
+
+  it('exposes an applications API instance', () => {
+    assert(api.applications instanceof ApplicationsAPI);
+  });
+
+  it('exposes an asterisk API instance', () => {
+    assert(api.asterisk instanceof AsteriskAPI);
+  });
+
+  it('exposes a bridges API instance', () => {
+    assert(api.bridges instanceof BridgesAPI);
+  });
+});
